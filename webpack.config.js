4eea6df7f23a18d7dea7cd8b365ae7d069367245
lance <861936154@qq.com>
2021-09-14 22:33:42 +0800

    完成webpack引入CSS 但使用插件提取CSS文件出錯

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: "./src/index.js",
+    devtool: 'inline-source-map',
+    performance: {
+      "maxEntrypointSize":10000000,
+      "maxAssetSize":30000000,
+    },
+    devServer: {
+      static: './dist',
+    },
     output: {
       filename: "[name].[contenthash].js", 
       },
       plugins: [new HtmlWebpackPlugin({
-        filename: "index.[contenthash].html",
+        filename: "index.html",
       template:"./src/test.html"}
-      )]
+      )],
+      module: {
+        rules: [
+        {
+          test: /\.css$/i,
+        //use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          use: ["style-loader","css-loader"],
+          //style-loader 将style标签加入head;css-loader只是将style引入js
+        },
+        ],
+      //   optimization: {
+      //     // minimizer: [
+      //     //   new CssMinimizerPlugin(),
+      //     // ],
+      //     minimize: true,
+      //  },
+      },
   };
-  
+
